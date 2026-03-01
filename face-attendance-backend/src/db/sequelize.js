2938465a1f0a08ea05d061ad8c8285f89b967619
tuandang23 <dangntce171876@fpt.edu.vn>
2026-03-01 19:29:36 +0700

    update code font-end of admin

--- face-attendance-backend/src/db/sequelize.js
+++ face-attendance-backend/src/db/sequelize.js
@@ -1,10 +1,12 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const sequelize = new Sequelize({
   host: process.env.PG_HOST || '127.0.0.1',
   port: parseInt(process.env.PG_PORT) || 5432,
   username: process.env.PG_USER || 'postgres',
-  password: process.env.PG_PASSWORD ? String(process.env.PG_PASSWORD) : '1',
+  password: process.env.PG_PASSWORD ? String(process.env.PG_PASSWORD) : '12345',
   database: process.env.PG_DATABASE || 'facedb',
   dialect: 'postgres',
   logging: false
